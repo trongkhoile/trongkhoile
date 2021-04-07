431754668f3c1bfb1668de2bf065dc73b0e206a7
trongkhoile <80833913+trongkhoile@users.noreply.github.com>
2021-04-07 23:13:37 +0700

    Add files via upload

---
+++ baibonus1.js
@@ -0,0 +1,3 @@
+let so= prompt("Nhập số ")
+var n= so.length;
+console.log('Số các chữ số là '+n)
---
+++ baibonus2.js
@@ -0,0 +1,4 @@
+let a= prompt("Nhập a ")
+let b= prompt("Nhập b")
+let x=-a/b
+console.log('Nghiệm của phương trình '+ a + "x" + "+" + b + "=" + "0 " + "là "  + x)
